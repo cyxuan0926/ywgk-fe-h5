43f523401d8f8a4c9dfe47749165a203a9f6260c
twmay006 <twmay006@163.com>
2018-07-31 10:12:30 +0800

    封面

--- src/app/common/index.vue
+++ src/app/common/index.vue
@@ -2,7 +2,7 @@
   <ul class="error">
     <li @click="goPage('/law/list?prisonerId=88')">法律法规</li>
     <li @click="goPage('/news/detail/1')">新闻详情</li>
-    <li @click="goPage('/prison/detail/2')">监狱详情</li>
+    <li @click="goPage('/prison/detail/98')">监狱详情</li>
   </ul>
 </template>
 
