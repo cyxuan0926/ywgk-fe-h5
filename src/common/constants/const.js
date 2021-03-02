cea3bab93b65abf284f4612ecac2a002132378ac
caiyang <caiyang@sinog2c.com>
2021-03-02 14:16:12 +0800

    feat: 甘肃广告配置

--- src/common/constants/const.js
+++ src/common/constants/const.js
@@ -2,6 +2,10 @@ import guangdongProvince from '@/assets/images/guangdong-province.png'
 
 import guangxiProvince from '@/assets/images/guangxi-province.png'
 
+import gansuLanzhou from '@/assets/images/gansu-lanzhou.png'
+
+import gansuNvzi from '@/assets/images/gansu-nvzi.png'
+
 export const noticeImgSwitchJaild = [
     {
         jaild: 20,
@@ -10,5 +14,13 @@ export const noticeImgSwitchJaild = [
     {
         jaild: 28,
         url: guangxiProvince
+    },
+    {
+        jaild: 87,
+        url: gansuLanzhou
+    },
+    {
+        jaild: 86,
+        url: gansuNvzi
     }
 ]
