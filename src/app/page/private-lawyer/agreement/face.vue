1f97261a0f4cf6dec8dce4394a109d64c64ef887
HUBI880626 <284432494@qq.com>
2021-11-30 11:31:41 +0800

    refactor：修改脸部协议正在定制中

--- src/app/page/private-lawyer/agreement/face.vue
+++ src/app/page/private-lawyer/agreement/face.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-        私人律师脸部图像采集协议正在定制中
+    <div class="face-container">
+        <p>私人律师脸部图像采集协议正在定制中...</p> 
     </div>
 </template>
 
@@ -20,4 +20,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .face-container {
+        width: 100%;
+        height: 60rem;
+        background: url('../../../../assets/images/download-bg-bak-1018.png') no-repeat 0 0;
+        background-size: 100% auto;
+
+        p {
+            font-size: 14px;
+            color: #999;
+            text-align: center;
+            padding-top: 9rem;
+            margin: 0;
+        }
+    }
 </style>
